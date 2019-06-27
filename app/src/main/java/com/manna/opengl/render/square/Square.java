8afbc9ad62bee527eb822dbc82a9357547d16686
yangtianci <15971476977@163.com>
2019-06-27 10:10:15 +0800

    OpenGL ES 添加GLSL着色器语言类型、申明参数解析

--- app/src/main/java/com/manna/opengl/render/square/Square.java
+++ app/src/main/java/com/manna/opengl/render/square/Square.java
@@ -14,6 +14,20 @@ public class Square {
     private FloatBuffer vertexBuffer;
     private ShortBuffer indexBuffer;
 
+    /**
+     * attribute：各个顶点不同的量
+     * vec4     ：vector 表示向量，vec4表示4维向量
+     * uniform  ：表示3D物体中顶点相同的量，例如光源位置、统一变换矩阵
+     * mat4     : Matrix矩阵，mat4表示4*4矩阵
+     * varying  ：顶点着色器传递到片元着色器的量
+     * precision：精度，用作申明
+     * mediump  : 浮点精度，lowp：低精度，8位；mediump：中精度，10位；highp：高精度，16位
+     * gl_Position ：顶点着色器内建变量，表示顶点坐标(gl_PointSize，表示点的大小，默认为1)
+     * gl_FragColor ：片元着色器内建变量,表示当前片元颜色
+     * gl_FragCoord：当前片元相对窗口位置所处的坐标
+     * gl_FragFacing：bool型，表示是否为属于光栅化生成此片元的对应图元的正面
+     * gl_FragData：vec4类型的数组,向其写入的信息，供渲染管线的后继过程使用
+     */
     private final String vertexShaderCode =
             "attribute vec4 vPosition;" +
                     "uniform mat4 vMatrix;" +
